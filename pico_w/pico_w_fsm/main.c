f2c9e35742d0e0e8593d8d932b6fcffbd3b18de1
Ryan Challacombe <ryanchallacombe@gmail.com>
2026-09-11 15:11:56 -0500

    added fsm structure to pico code

--- pico_w/pico_w_fsm/main.c
+++ pico_w/pico_w_fsm/main.c
@@ -12,10 +12,26 @@
 #include "pico/binary_info.h"
 #include "include/wifi_info.h"
 
+/**********************************
+ *  FSM STATES
+**********************************/
+enum State : uint16_t
+{
+    S_INITIALIZE,
+    S_LOOP,
+    S_ERROR
+};
 
 /**********************************
- *  LIS3DH SETTINGS
+ *  GLOBALS
 **********************************/
+enum State currentState, nextState;
+
+bool s_initialize_fail = false;
+
+/*******************
+ *  LIS3DH SETTINGS
+********************/
 // LIS3DH address
 const int ADDRESS = 0x18;
 
@@ -57,9 +73,210 @@ const uint8_t SET_INT1_THS = 0x6E;      // 110 LSBs, ref table 59
 const uint8_t INT1_DURATION = 0x33;
 const uint8_t SET_INT1_DURATION = 0x01;  // 1 decimal, ref table 61
 
+float x_accel, y_accel, z_accel;
+bool int_triggered = false;
+const uint8_t int1_src_addr = INT1_SRC;
+uint8_t int1_src = 0;
+
+/**********************************
+ *  FUNCTION PROTOTYPES
+**********************************/
+void lis3dh_init();
+void lis3dh_calc_value(uint16_t raw_value, float *final_value, bool isAccel);
+void lis3dh_read_data(uint8_t reg, float *final_value, bool IsAccel);
+void uponEnter( const enum State state );
+void updateStateMachine( const enum State state );
+void uponExit( const enum State state );
+const char *stateToString ( const enum State state );
+
+/**********************************
+ *  MAIN
+**********************************/
+int main()
+{
+    currentState = nextState = S_INITIALIZE;
+
+    uponEnter(currentState);
+
+    printf("Starting while() loop ....");
+
+    while(true) {
+        // Udate FSM
+        updateStateMachine( currentState );
+
+        // Handle State Transitions
+        if ( nextState != currentState ) {
+            uponExit( currentState );
+            uponEnter( nextState );
+            currentState = nextState;   
+        } 
+
+    } // while
+}   // main
+
 /**********************************
  *  FUNCTION DEFINITIONS
 **********************************/
+
+void uponEnter( const enum State state ) {
+    printf("Entering state: %s\n", stateToString(state));
+
+    switch (state) {
+        case S_INITIALIZE:
+        {
+            /**********************************
+             *  GENERAL SETUP
+             **********************************/
+            stdio_init_all();
+            busy_wait_ms(5000);     // wait for USB serial
+
+            /**********************************
+             *  WIFI SETUP
+             **********************************/
+            // Initialise the Wi-Fi chip
+            if (cyw43_arch_init()) {
+                printf("Wi-Fi init failed\n");
+                s_initialize_fail = true;
+            }
+
+            // Enable wifi station
+            cyw43_arch_enable_sta_mode();
+
+            printf("Connecting to Wi-Fi...\n");
+            if (cyw43_arch_wifi_connect_timeout_ms(wifi_ssid, wifi_pw, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+                printf("failed to connect.\n");
+                // TODO handle this differently
+                s_initialize_fail = true;
+            } else {
+                printf("Connected.\n");
+                // Read the ip address in a human readable way
+                uint8_t *ip_address = (uint8_t*)&(cyw43_state.netif[0].ip_addr.addr);
+                printf("IP address %d.%d.%d.%d\n", ip_address[0], ip_address[1], ip_address[2], ip_address[3]);
+            }
+
+            busy_wait_ms(500);
+
+            /**********************************
+             *  I2C SETUP
+            *********************************/
+            // Use I2C0 on the default SDA and SCL pins (4, 5 on a Pico)
+            i2c_init(i2c_default, 400 * 1000);
+            gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+            gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+            gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+            gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+            // Make the I2C pins available to picotool
+            bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
+            lis3dh_init();
+
+            break;
+        }
+
+        case S_LOOP:
+            break;
+        case S_ERROR:
+            break;
+        default:
+            break;
+    }
+
+}
+
+void updateStateMachine( const enum State state ) {
+    printf("updateStateMachine state: %s\n", stateToString(state));
+
+    switch (state) {
+        case S_INITIALIZE:
+        {
+            if (s_initialize_fail)
+            {
+                // go to error state
+
+            }
+            else
+            {
+                nextState = S_LOOP;
+            }
+            break;
+        }
+
+        case S_LOOP:
+        {
+            lis3dh_read_data(0x28, &x_accel, true);
+            lis3dh_read_data(0x2A, &y_accel, true);
+            lis3dh_read_data(0x2C, &z_accel, true);
+
+            if (int_triggered) {
+                printf("An interrupt has been triggered since main() began\n");
+            }
+
+            // check if interrupt has been triggered
+            int1_src = 0;
+            i2c_write_blocking(i2c_default, ADDRESS, &int1_src_addr, 1, true);
+            i2c_read_blocking(i2c_default, ADDRESS, &int1_src, 1, false);
+            if (int1_src & 0x40) {
+                int_triggered = true;
+                printf("Interrupt triggered!\n");
+            }
+
+            // Display data 
+            // Acceleration is read as a multiple of g (gravitational acceleration on the Earth's surface)
+            printf("ACCELERATION VALUES: \n");
+            printf("X acceleration: %.3fg\n", x_accel);
+            printf("Y acceleration: %.3fg\n", y_accel);
+            printf("Z acceleration: %.3fg\n", z_accel);
+
+            sleep_ms(500);
+
+            // Clear terminal 
+            printf("\033[1;1H\033[2J");
+
+            break;
+        }
+        case S_ERROR:
+            break;
+        default:
+            break;
+    }   // switch
+}
+
+void uponExit( const enum State state ) {
+    printf("Exiting state: %s\n", stateToString(state));
+
+    switch (state) {
+        case S_INITIALIZE:
+            // nothing to do on exit
+            break;
+        case S_LOOP:
+            // nothing to do on exit
+            break;
+        case S_ERROR:
+            break;
+        default:
+            break;
+    }
+
+}
+
+const char *stateToString ( const enum State state ) {
+    const char *stateStr;     // this is a string literal
+
+    switch (state) {
+        case S_INITIALIZE:
+            stateStr = "S_INITIALIZE";
+            break;
+        case S_LOOP:
+            stateStr = "S_LOOP";
+            break;
+        case S_ERROR:
+            stateStr = "S_ERROR";
+            break;
+        default:
+            break;
+    }
+    return stateStr;
+}
+
 void lis3dh_init() {
     uint8_t buf[2];
 
@@ -130,92 +347,4 @@ void lis3dh_read_data(uint8_t reg, float *final_value, bool IsAccel) {
     raw_accel = (msb << 8) | lsb;
 
     lis3dh_calc_value(raw_accel, final_value, IsAccel);
-}
-
-int main()
-{
-    /**********************************
-     *  GENERAL SETUP
-     **********************************/
-    stdio_init_all();
-
-    busy_wait_ms(5000);     // wait for USB serial
-
-    /**********************************
-     *  WIFI SETUP
-     **********************************/
-
-    // Initialise the Wi-Fi chip
-    if (cyw43_arch_init()) {
-        printf("Wi-Fi init failed\n");
-        return -1;
-    }
-
-    // Enable wifi station
-    cyw43_arch_enable_sta_mode();
-
-    printf("Connecting to Wi-Fi...\n");
-    if (cyw43_arch_wifi_connect_timeout_ms(wifi_ssid, wifi_pw, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
-        printf("failed to connect.\n");
-        return 1;
-    } else {
-        printf("Connected.\n");
-        // Read the ip address in a human readable way
-        uint8_t *ip_address = (uint8_t*)&(cyw43_state.netif[0].ip_addr.addr);
-        printf("IP address %d.%d.%d.%d\n", ip_address[0], ip_address[1], ip_address[2], ip_address[3]);
-    }
-
-    busy_wait_ms(5000);
-
-    /**********************************
-     *  I2C SETUP
-    *********************************/
-   
-    // This example will use I2C0 on the default SDA and SCL pins (4, 5 on a Pico)
-    i2c_init(i2c_default, 400 * 1000);
-    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
-    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
-    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
-    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
-    // Make the I2C pins available to picotool
-    bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
-
-    float x_accel, y_accel, z_accel;
-
-    lis3dh_init();
-    bool int_triggered = false;
-    const uint8_t int1_src_addr = INT1_SRC;
-    uint8_t int1_src = 0;
-
-    printf("Starting while() loop ....");
-    while (true) {
-        lis3dh_read_data(0x28, &x_accel, true);
-        lis3dh_read_data(0x2A, &y_accel, true);
-        lis3dh_read_data(0x2C, &z_accel, true);
-
-        if (int_triggered) {
-            printf("An interrupt has been triggered since main() began\n");
-        }
-
-        // check if interrupt has been triggered
-        int1_src = 0;
-        i2c_write_blocking(i2c_default, ADDRESS, &int1_src_addr, 1, true);
-        i2c_read_blocking(i2c_default, ADDRESS, &int1_src, 1, false);
-        if (int1_src & 0x40) {
-            int_triggered = true;
-            printf("Interrupt triggered!\n");
-        }
-
-        // Display data 
-        // Acceleration is read as a multiple of g (gravitational acceleration on the Earth's surface)
-        printf("ACCELERATION VALUES: \n");
-        printf("X acceleration: %.3fg\n", x_accel);
-        printf("Y acceleration: %.3fg\n", y_accel);
-        printf("Z acceleration: %.3fg\n", z_accel);
-
-        sleep_ms(500);
-
-        // Clear terminal 
-        printf("\033[1;1H\033[2J");
-    }
 }
